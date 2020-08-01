f1593b26b45066681834179b7a9d1ce21c49d0be
beaock <beaock@gmail.com>
2020-08-01 13:21:54 +0900

    开发基本UI菜单

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// 
@@ -19,14 +20,33 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    // 开始新游戏
+    public void NewGame()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene(1);
+    }
+    //重新加载当前场景
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+       // PlayerPrefs.DeleteKey("playerHealth");
+    }
+
+    //暂停游戏
     public void PauseGame()
     {
         Time.timeScale = 0;
 
     }
-
+    // 回到游戏
     public void ResumeGame()
     {
         Time.timeScale = 1;
+    }
+    //返回主菜单
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/// <summary>
+/// 
+/// </summary>
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    [Header("UI Elements")]
+    public GameObject pauseMenu;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PauseGameAndShowMenu() //button method
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+        
+    }
+
+    public void ResumeGameAndCloseMenu() //button method
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(false);
+
+    }
+}
